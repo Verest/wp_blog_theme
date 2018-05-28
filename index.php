e8d57ca9b5a3122682514ac753ac155c4b29379e
Richie Black <rb15243@gmail.com>
2018-05-28 11:56:48 -0700

    added starter wp loop to index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php get_header(); ?>
-stuff
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+  <h3 class="reb_title"><?php the_title(); ?></h3>
+  <?php the_content();  ?>
+<?php endwhile; else:  ?>
+  No Posts Yet
+<?php endif; ?>
 <?php get_footer(); ?>
