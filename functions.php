4871c0dfb650eb223973059a42332c3c049703b3
Richie Black <rb15243@gmail.com>
2018-05-28 12:16:29 -0700

    set up typography (vertical rhythm)

--- functions.php
+++ functions.php
@@ -10,3 +10,6 @@ add_action('wp_enqueue_scripts', 'reb_theme_js');
 function reb_theme_js(){
   wp_enqueue_script("nav_js", get_template_directory_uri()."/hamburger.js", '', '', true);
 }
+
+//remove admin bar
+add_filter('show_admin_bar', '__return_false');
