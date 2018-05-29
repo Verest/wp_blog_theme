3399120393a66418142afcdf58344ebf568da2de
Richie Black <rb15243@gmail.com>
2018-05-29 11:30:44 -0700

    added category.php archive file

---
+++ category.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+<div class="main_content">
+  <h2>Category: <?php single_cat_title(" "); ?></h2>
+
+  <?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <a href="<?php the_permalink() ?>">
+      <h3 class="reb_title"><?php the_title(); ?></h3>
+    </a>
+    <?php the_content();  ?>
+  <?php endwhile; else:  ?>
+    No Posts Yet
+  <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
