bb55d21d22aa1573114b4681da69a19d6d6a0870
Richie Black <rb15243@gmail.com>
2018-05-29 11:28:39 -0700

    added date.php archive template, fixed some sidebar css issues

--- date.php
+++ date.php
@@ -3,7 +3,8 @@
 <?php get_sidebar(); ?>
 
 <div class="main_content">
-  <h2>Recent Posts</h2>
+  <h2>Posts from: <?php single_month_title(" "); ?></h2>
+
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <a href="<?php the_permalink() ?>">
       <h3 class="reb_title"><?php the_title(); ?></h3>
