b7b56a092801312c7ae1667fc6f335d71c54b198
Richie Black <rb15243@gmail.com>
2018-05-29 11:18:34 -0700

    added simple single post template & links to front-page

--- single.php
+++ single.php
@@ -4,9 +4,7 @@
 
 <div class="main_content">
   <?php if(have_posts()): while(have_posts()): the_post(); ?>
-    <a href="<?php the_permalink() ?>">
-      <h3 class="reb_title"><?php the_title(); ?></h3>
-    </a>
+    <h3 class="reb_title"><?php the_title(); ?></h3>
     <?php the_content();  ?>
   <?php endwhile; else:  ?>
     No Posts Yet
