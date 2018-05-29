60aec3ac3f41ef9dcdfc06dfe05e6dda853b379e
Richie Black <rb15243@gmail.com>
2018-05-29 12:17:17 -0700

    added search.php

---
+++ search.php
@@ -0,0 +1,23 @@
+<?php
+  global $query_string;
+
+  wp_parse_str( $query_string, $search_query  );
+?>
+
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+<div class="main_content">
+  <h2>Search Results: <?php the_search_query(); ?></h2>
+
+  <?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <a href="<?php the_permalink() ?>">
+      <h3 class="reb_title"><?php the_title(); ?></h3>
+    </a>
+    <?php the_content();  ?>
+  <?php endwhile; else:  ?>
+    No Posts Yet
+  <?php endif; ?>
+
+<?php get_footer(); ?>
